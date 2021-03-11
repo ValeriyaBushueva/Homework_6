6c36a9d435965ee744dc5516266d902e5d713e8f
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-03-11 16:48:30 +0300

    homework6

---
+++ StudentMethods.cs
@@ -0,0 +1,46 @@
+﻿namespace Homework_6
+{
+    public static class StudentMethods
+    {
+        public static bool IsLastCourse(Student student)
+        {
+            if (student.course == 6)
+                return true;
+
+            return false;
+        }
+
+        public static bool IsCityMoscow(Student student)
+        {
+            if (student.city == "Москва")
+                return true;
+
+            return false;
+        }
+        
+        public static bool IsCityMinsk(Student student)
+        {
+            if (student.city == "Минск")
+                return true;
+
+            return false;
+        }
+        
+        public static bool IsUniverBNTY(Student student)
+        {
+            if (student.university == "БНТУ")
+                return true;
+
+            return false;
+        }
+        
+        public static bool IsFirsCourse(Student student)
+        {
+            if (student.course == 1)
+                return true;
+
+            return false;
+        }
+        
+    }
+}
